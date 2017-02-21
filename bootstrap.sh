60b80025e4051db782502e4463648b0473ba9fd0
Naoaki Obiki <obiki.wk@gmail.com>
2017-02-21 16:41:06 +0900

    modified:   bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,8 @@ if [ ! -e /bootstrap.lock ]; then
   /git-daemon.sh
 
   cp /usr/local/lib/satis/satis.json.org /repos/satis.json
+  mkdir -p /repos/satis/
+  chown 1000:1000 /repos/satis/
 
   touch /bootstrap.lock
 fi
